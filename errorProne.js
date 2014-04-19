c9cf4afc94c680ccaf76fba509d5e792146f3ca0
Dan Fox <iamdanfox@gmail.com>
2014-04-19 18:25:43 +0100

    Queuing works nicely (481 bytes)

--- errorProne.js
+++ errorProne.js
@@ -10,13 +10,67 @@
       };
       return this.each(function() {
         return $(this).queue(function() {
-          var elem;
-          console.log('f called');
+          var append, attr, backsp, elem, typeTo;
           elem = this;
-          return setTimeout(function() {
-            elem.value += txt;
-            return $(elem).dequeue();
-          }, Math.random() * 3000);
+          attr = elem.tagName === 'input'.toUpperCase() || elem.tagName === 'textarea'.toUpperCase() ? 'value' : 'innerHTML';
+          append = function(str, cont) {
+            if (str) {
+              elem[attr] += str[0];
+              setTimeout((function() {
+                return append(str.slice(1), cont);
+              }), 100);
+            } else {
+              cont();
+            }
+          };
+          backsp = function(num, cont) {
+            if (num) {
+              elem[attr] = elem[attr].slice(0, -1);
+              setTimeout((function() {
+                return backsp(num - 1, cont);
+              }), 100);
+            } else {
+              cont();
+            }
+          };
+          return (typeTo = function(i) {
+            var afterErr, r;
+            if (txt.length >= i) {
+              r = Math.random();
+              afterErr = function() {
+                return setTimeout((function() {
+                  return typeTo(i);
+                }), interval(i));
+              };
+              if (0.04 * 0.3 > r && txt[i - 1] !== txt[i]) {
+                append(txt.slice(i, i + 4), function() {
+                  return backsp(4, afterErr);
+                });
+              } else if (0.04 * 0.5 > r && txt[i - 1] !== txt[i]) {
+                append(txt[i], function() {
+                  return backsp(1, afterErr);
+                });
+              } else if (0.04 * 0.8 > r && txt[i - 1] !== txt[i]) {
+                append(txt[i] + txt[i - 1], function() {
+                  return backsp(2, afterErr);
+                });
+              } else if (0.04 * 1.0 > r && i > 1 && txt[i - 2] === txt[i - 2].toUpperCase()) {
+                append(txt[i - 1].toUpperCase() + txt.slice(i, i + 4), function() {
+                  return backsp(5, afterErr);
+                });
+              } else {
+                elem[attr] += txt[i - 1];
+                if (keypress) {
+                  keypress.call(elem, i);
+                }
+                setTimeout((function() {
+                  return typeTo(i + 1);
+                }), interval(i));
+              }
+            } else {
+              $(elem).dequeue();
+            }
+          })(1);
         });
       });
     }
